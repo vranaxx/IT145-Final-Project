223fdc29dbacedee7b1bd77166722c4ab57fb7c5
vranaxx <vranaxx120@gmail.com>
2026-03-23 10:06:28 -0400

    Add files via upload

---
+++ IT145_FinalProject/Gacha.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT145_FinalProject
+{
+    internal class Gacha
+    {
+    }
+}
---
+++ IT145_FinalProject/TitleForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace IT145_FinalProject
+{
+    public partial class TitleForm : Form
+    {
+        private Character createdChar;
+        public TitleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createcharButton_Click(object sender, EventArgs     e)
+        {
+            if (professionCombo.Text == "")
+            {
+                MessageBox.Show("Error: Empty Selection");
+                return;
+            }
+
+            string tempNameSelection = charnameTxt.Text;
+            string professionSelection = professionCombo.Text;
+            Profession tempProfession = new Profession(professionSelection);
+
+            createdChar = new Character(tempProfession, tempNameSelection);
+
+            MessageBox.Show("Character created successfully!");
+
+        }
+
+        private void displayprofstatButton_Click(object sender, EventArgs e)
+        {
+            if (createdChar == null)
+            {
+                MessageBox.Show("Error: No character created yet!");
+                return;
+            }
+
+            MessageBox.Show(
+                $"Name: {createdChar.GetCharacterName()}\n" +
+                $"Profession: {createdChar.GetProfessionName()}\n" +
+                $"ATK: {createdChar.GetProfessionATK()}\n" +
+                $"HP: {createdChar.GetProfessionHP()}\n" +
+                $"INT: {createdChar.GetProfessionINT()}"
+                );
+        }
+    }
+}
