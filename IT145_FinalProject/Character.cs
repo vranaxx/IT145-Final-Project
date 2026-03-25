8dab5d05fef48afa3e4c67c12ecdf14b4cb0cfb3
vranaxx <vranaxx120@gmail.com>
2026-03-25 12:34:49 -0400

    Add files via upload

---
+++ IT145_FinalProject/Character.cs
@@ -0,0 +1,146 @@
+﻿/*
+ * Program: Character Class
+ * Purpose: Class to store character information
+ * Author: Thomas Shampney
+ * Date: 2026-03-20
+ */
+
+#region To Do
+
+/*
+ * To Do
+ * - Error checking and deletion of old GUID in GenereateNewGUID function
+ * - Error checking for other set functions
+ */
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+
+namespace IT145_FinalProject
+{
+    internal class Character
+    {
+        protected string _characterName; //TBS: Character name
+        protected Guid _characterID; //TBS: Character identifier
+        protected int _characterHP; //TBS: Stat for character health scaling
+        protected int _characterATK; //TBS: Stat for damage scaling
+        protected int _characterINT; //TBS: Stat for secondary/passive/skill scaling
+        Profession _characterProfession; //TBS: Character profession inherit
+
+        private HashSet<Guid> _professionGUID = new HashSet<Guid>(); //TBS: HashSet for GUIDs
+
+        #region Constructors
+        public Character() //TBS: Default Constructor
+        {
+            _characterID = Guid.NewGuid();
+            _characterName = "";
+            _characterATK = 0;
+            _characterHP = 0;
+            _characterATK = 0;
+        } 
+
+        public Character(string charName, int charHP, int charATK, int charINT, Profession charProfession) //TBS: Full Constructor
+        {
+            _characterID = Guid.NewGuid();
+            _characterProfession = charProfession;
+            _characterName = charName;
+            _characterATK = charProfession.GetProfessionATK();
+            _characterINT = charProfession.GetProfessionINT();
+            _characterHP = charProfession.GetProfessionHP();
+        }
+        public Character(Profession charProfession) //TBS: Class only constructor
+        {
+            _characterID = Guid.NewGuid();
+            _characterProfession = charProfession;
+            _characterHP = charProfession.GetProfessionHP();
+            _characterATK = charProfession.GetProfessionATK();
+            _characterINT = charProfession.GetProfessionINT();
+
+        }
+        public Character(Profession charProfession, string charName) //TBS: Class only constructor
+        {
+            _characterName = charName;
+            _characterID = Guid.NewGuid();
+            _characterProfession = charProfession;
+            _characterHP = charProfession.GetProfessionHP();
+            _characterATK = charProfession.GetProfessionATK();
+            _characterINT = charProfession.GetProfessionINT();
+
+        }
+        public Character(string charName, int charHP, int charATK, int charINT) //TBS: Stat only constructor
+        {
+            _characterID = Guid.NewGuid();
+            _characterName = charName;
+            _characterATK = charATK;
+            _characterINT = charINT;
+            _characterHP = charHP;
+        }
+
+        #endregion
+
+        #region Get Set
+        public void CharacterSetClass (Profession charProfession) //TBS: Set character class
+        {
+            _characterProfession = charProfession; 
+        }
+
+        public void CharacterSetName(string name) //TBS: Set Name Functiion
+        {
+            _characterName = name;
+        }
+
+        public void CharacterGenNewGUID() //TBS: Set ID Functiion
+        {
+            _characterID = Guid.NewGuid();
+        }
+        public void CharacterSetATK(int ATK) //TBS: Set ATK Functiion   
+        {
+            _characterATK = ATK;
+        }
+        public void CharacterSetHP(int HP) //TBS: Set HP Functiion
+        {
+            _characterHP = HP;
+        }
+        public void CharacterSetINT(int INT) //TBS: Set INT Functiion
+        {
+            _characterINT = INT;
+        }
+
+        public void AddGUID (Character GUID) //TBS: Function to manually add a GUID to HashSet
+        {
+            _professionGUID.Add(GUID._characterID);
+        }
+        public string GetProfessionName() //TBS: Get Function for Name
+        {
+            return _characterProfession.GetProfessionName();
+        }
+
+        public int GetProfessionATK() //TBS: Get Function for ATK
+        {
+            return _characterProfession.GetProfessionATK();
+        } 
+         
+
+        public int GetProfessionHP() //TBS: Get Function for HP
+        {
+            return _characterProfession.GetProfessionHP();
+        }
+
+        public int GetProfessionINT() //TBS: Get Function for INT
+        {
+            return _characterProfession.GetProfessionINT();
+        }
+
+        public string GetCharacterName() //TBS: Get Function for character name
+        {
+            return _characterName;
+        }
+
+        #endregion
+    }
+}
---
+++ IT145_FinalProject/Profession.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Program: Profession Class
+ * Purpose: Class to define what a profession is for use in character class
+ * Author: Thomas Shampney
+ * Date: 2026-03-20
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT145_FinalProject
+{
+    internal class Profession
+    {
+        // private HashSet<String> _professionNames = new HashSet<String>() { "Warrior", "Tank", "Rogue" }; //TBS: HashSet for class names
+
+        private string _professionName; //TBS: Variable for class name; also used as UID
+        private int _professionINT; //TBS: Profession base INT stat
+        private int _professsionHP; //TBS: Profession base HP stat
+        private int _professionATK; //TBS: Profession base ATK stat
+
+        public Profession() //TBS: Defaault Constructor
+        {
+
+        }
+        public Profession(string name) //TBS: Constructor based on class
+        {
+            switch (name)
+            {
+                case "Warrior":
+                    _professionName = "Warrior";
+                    _professionATK = 5;
+                    _professionINT = 2;
+                    _professsionHP = 3;
+                    break;
+                case "Tank":
+                    _professionName = "Tank";
+                    _professionATK = 3;
+                    _professionINT = 2;
+                    _professsionHP = 5;
+                    break;
+                case "Ranged":
+                    _professionName = "Ranged";
+                    _professionATK = 4;
+                    _professionINT = 4;
+                    _professsionHP = 2;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public string GetProfessionName()
+        {
+            return _professionName; 
+        }
+        public int GetProfessionINT()
+        {
+            return _professionINT;
+        }
+
+        public int GetProfessionHP()
+        {
+            return _professsionHP;
+        }
+
+        public int GetProfessionATK()
+        {
+            return _professionATK;
+        }
+    }
+}
